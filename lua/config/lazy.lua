7ab7eab7e3f7dacbb1b639c66e9e43e617c2993d
TJ <tom@guerrilla.com.au>
2025-11-03 09:08:31 +1000

    move background transp code

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -118,16 +118,17 @@ vim.opt.updatetime = 50
 
 vim.opt.autoread = true
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = {
         {
             "rose-pine/neovim",
-            config = function() vim.cmd.colorscheme "rose-pine" end
+            config = function() 
+                vim.cmd.colorscheme "rose-pine" 
+                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+                vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+            end
         },
         -- import your plugins
         { import = "plugins" },
