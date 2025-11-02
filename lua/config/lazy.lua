fcc897b1cf8b3ec7af368d8e33375257d6714415
TJ <tom@guerrilla.com.au>
2025-11-03 09:10:57 +1000

    Allow floats to have a background.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -126,7 +126,6 @@ require("lazy").setup({
             config = function() 
                 vim.cmd.colorscheme "rose-pine" 
                 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
                 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
             end
         },
